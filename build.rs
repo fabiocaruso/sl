fd2811409c8cde4be53950f6ada962f34bf54a80
Fabio Caruso <fabio.caruso@hotmail.com>
2020-11-21 12:41:10 +0100

    Update build.rs

--- build.rs
+++ build.rs
@@ -4,7 +4,12 @@ use std::path::Path;
 
 fn main() {
     if env::var("PROFILE").unwrap() == "release" {
-        Command::new("npm").args(&["run", "build"])
+        if !Path::new(&[&env::var("CARGO_MANIFEST_DIR").unwrap(), "/frontend/soundloop/node_modules"].concat()).exists() {
+            Command::new("npm").arg("install")
+                .current_dir(&[&env::var("CARGO_MANIFEST_DIR").unwrap(), "/frontend/soundloop"].concat())
+                .status().unwrap();
+        }
+        Command::new("npm").arg("run").arg("build")
             .current_dir(&[&env::var("CARGO_MANIFEST_DIR").unwrap(), "/frontend/soundloop"].concat())
             .status().unwrap();
 
